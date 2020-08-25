cafed420545f9364abaf21605e11f185d6ba67b0
teoyi <yipengteo@yahoo.com.sg>
2020-08-24 22:42:57 -0700

    Completed

--- javascript-exercises/removeFromArray/removeFromArray.js
+++ javascript-exercises/removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-asdasdas
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray
 }
 
 module.exports = removeFromArray
