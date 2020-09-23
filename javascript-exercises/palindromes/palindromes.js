c74e83bb465febc5ea4cd87a2ba9f67672ce16f8
teoyi <yipengteo@yahoo.com.sg>
2020-09-23 15:41:48 -0700

    Completed

--- javascript-exercises/palindromes/palindromes.js
+++ javascript-exercises/palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    modString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+    return (
+        modString 
+            .split("")
+            .reverse()
+            .join("") === modString
+    );
 }
 
 module.exports = palindromes
