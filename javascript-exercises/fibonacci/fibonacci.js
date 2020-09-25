1ca5633a18fb8b822c115f3a3b8321e9ae800753
teoyi <yipengteo@yahoo.com.sg>
2020-09-24 21:18:33 -0700

    Inital attmept

--- javascript-exercises/fibonacci/fibonacci.js
+++ javascript-exercises/fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    //Taking into account of values below 0 
+    if (num < 0) return ('OOPS');
+    if (num === 0) return 0;
 
+    //Starting the fibonacci sequence, with two numbers 0 and 1
+    let a = 0;
+    let b = 1; 
+    for (i = 1; i < num; i++) {
+        
+    }
 }
 
 module.exports = fibonacci
