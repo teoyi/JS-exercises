cfe9d4ac8bea4eb996dd20e7f167c3fec28595b2
teoyi <yipengteo@yahoo.com.sg>
2020-10-29 01:44:32 -0700

    Finsihed second last exercise, learnt about in place algorithm

--- Eloquent-JS-exercises/chapter3-4.js
+++ Eloquent-JS-exercises/chapter3-4.js
@@ -111,6 +111,31 @@ function reverseArray(array) {
 }
 console.log(reverseArray([1,2,3,4,5]))
 
-function reverseArrayInPlace() {
+function reverseArrayInPlace(array) {
+    for (i = 0; i < Math.floor(array.length/2); i++) { 
+        let temp = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = temp; 
+    }
+    return array;
+}
+
+console.log(reverseArrayInPlace([1,2,3,4,5]))
+
+// EX3: A list 
+
+function arrayToList(array) {
+
+}
+
+function listToArray(list) {
+
+}
+
+function prepend(element, list) {
+
+}
+
+function nth(list, n) {
 
 }
